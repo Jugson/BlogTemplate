de91e0482b2dd88b6bc6f9252c3bed2210825888
munganga@TOSHIBA-PC <jugson>
2021-02-02 21:33:16 +0100

    BlogTemplate

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class AdminController extends AbstractController
+{
+    /**
+     * @Route("/admin", name="admin")
+     */
+    public function index(): Response
+    {
+        return $this->render('admin/admin.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+    }
+}
